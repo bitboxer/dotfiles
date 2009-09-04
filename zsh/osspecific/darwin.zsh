d2e8dfd295180e1556fce34369520b9a6d5f5cf1
Bodo Tasche <bodo@wannawork.de>
2009-09-04 17:41:59 +0200

    beginning of separation between linux and mac

---
+++ zsh/osspecific/darwin.zsh
@@ -0,0 +1,10 @@
+export EDITOR='mate -w'
+
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export M2_HOME=/usr/local/apache-maven
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+
+export JDK_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
