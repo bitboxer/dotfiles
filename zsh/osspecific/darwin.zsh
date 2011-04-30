f425eee6b9510f0a8c55d3b5a1712de6557eb476
Bodo Tasche <bodo@wannawork.de>
2011-04-30 08:55:21 +0200

    moved rvm env to zshenv for macvim

--- zsh/osspecific/darwin.zsh
+++ zsh/osspecific/darwin.zsh
@@ -14,5 +14,3 @@ export JDK_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 
 export PYTHONPATH="/usr/local/lib/python2.6/site-packages/:$PYTHONPATH"
-
-if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
---
+++ zshenv
@@ -0,0 +1,4 @@
+# RVM-Integration must be done in zshenv for vim
+# http://beginrescueend.com/integration/vim/
+
+if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
