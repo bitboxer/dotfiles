1b40d5d4b7690b3e4b57b3536a694fdb6a25eeb2
Bodo Tasche <bodo@wannawork.de>
2012-01-15 12:05:39 +0100

    Removed outdated paths and variables from zsh

--- zsh/osspecific/darwin.zsh
+++ zsh/osspecific/darwin.zsh
@@ -3,14 +3,8 @@ alias oa='open -a' # App Launcher
 
 export EDITOR='mvim -f'
 
-export PATH="~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/git/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-export M2_HOME=/usr/local/apache-maven
-export M2=$M2_HOME/bin
-export PATH=$M2:$PATH
+export PATH="~/bin:/usr/local/bin:$PATH"
+export MANPATH="/usr/local/man:$MANPATH"
 
 export JDK_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-
-export PYTHONPATH="/usr/local/lib/python2.6/site-packages/:$PYTHONPATH"
--- zsh/osspecific/linux.zsh
+++ zsh/osspecific/linux.zsh
@@ -1,7 +1,2 @@
 alias ls='ls --color=auto'
-
 export EDITOR='vi'
-export PATH=$PATH$:/opt/ant/bin:/opt/maven2/bin
-
-export JAVA_HOME=/usr/lib/jvm/java-6-sun
-export JDK_HOME=/usr/lib/jvm/java-6-sun
