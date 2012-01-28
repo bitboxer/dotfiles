78c8f8a2647d7a50a7646f679bfcbc24573625ce
Bodo Tasche <bodo@wannawork.de>
2012-01-28 09:43:02 +0100

    added sbin path

--- zsh/osspecific/darwin.zsh
+++ zsh/osspecific/darwin.zsh
@@ -3,7 +3,7 @@ alias oa='open -a' # App Launcher
 
 export EDITOR='mvim -f'
 
-export PATH="~/bin:/usr/local/bin:$PATH"
+export PATH="~/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
 export JDK_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
