ea84ce5da64a0ed4f0d284031f82117cebfbb68d
Bodo Tasche <bodo@wannawork.de>
2013-10-11 15:33:44 +0200

    added grc to my config

--- tilde/config/fish/config.fish
+++ tilde/config/fish/config.fish
@@ -71,3 +71,19 @@ function fish_title
   end
   pwd
 end
+
+# GRC add color the output (thanks to @klaustopher)
+set GRC (which grc)
+alias colourify="$GRC -es --colour=auto"
+alias configure='colourify ./configure'
+alias diff='colourify diff'
+alias make='colourify make'
+alias gcc='colourify gcc'
+alias g++='colourify g++'
+alias as='colourify as'
+alias gas='colourify gas'
+alias ld='colourify ld'
+alias netstat='colourify netstat'
+alias ping='colourify ping'
+alias traceroute='colourify /usr/sbin/traceroute'
+
