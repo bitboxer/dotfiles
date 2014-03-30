ab3273af8677ed2b92d27b981119e6314e51abb0
Bodo Tasche <bodo@wannawork.de>
2014-03-30 15:48:14 +0200

    moved paths to other file

---
+++ tilde/zsh/paths.sh
@@ -0,0 +1,10 @@
+# NPM Paths
+export PATH=/usr/local/share/npm/bin/:$PATH
+
+# Homebrew must be first in PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:$PATH
+
+# Added local bin to path for binstubs
+export PATH=~/bin/:$PATH
+export PATH=./bin/:$PATH
