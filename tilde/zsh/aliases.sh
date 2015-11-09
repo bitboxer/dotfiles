10857adfce1697979e46776f830a736280d457bc
Bodo Tasche <bodo@wannawork.de>
2015-11-09 15:53:38 +0100

    forgot to make the git rebase alias interactive

--- tilde/zsh/aliases.sh
+++ tilde/zsh/aliases.sh
@@ -21,7 +21,7 @@ alias vim="nvim"
 alias vi="nvim"
 
 alias rb="git rebase $@"
-alias rbm="git rebase master"
+alias rbm="git rebase master -i"
 alias rbc="git rebase --continue"
 alias rba="git rebase --abort"
 
