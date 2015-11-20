597e9bee470dbacbe84d48878eaa8f62e4dd5f9a
Bodo Tasche <bodo@wannawork.de>
2015-11-20 19:31:07 +0100

    sadly had to revoke using git-up :sob:

--- tilde/zsh/aliases.sh
+++ tilde/zsh/aliases.sh
@@ -35,7 +35,8 @@ alias gs='git status'
 alias gd='git diff'
 alias glog='git log'
 alias gca='git commit -v -a'
-alias gl='git up'
+# Till this (https://github.com/aanand/git-up/issues/114) is fixed, i keep using git pull :(
+alias gl='git pull'
 alias gp='git push'
 alias gpb='git push --set-upstream origin HEAD' # Push local branch to remote
 alias gco='git checkout'
