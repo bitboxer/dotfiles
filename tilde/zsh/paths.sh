459b395982be00351ad705894d6099d6a164312c
Bodo Tasche <bodo@wannawork.de>
2015-11-22 09:48:19 +0100

    Addind several scripts to the dotfiles
    I use this scripts quiet a lot and just realized that they aren't vesion controlled : open_mouth: .
    
    This fixes it.

--- tilde/zsh/paths.sh
+++ tilde/zsh/paths.sh
@@ -5,6 +5,9 @@ export PATH=/usr/local/share/npm/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 
+# scripts from this repo
+export PATH=~/.bin:$PATH
+
 # Added local bin to path for binstubs
 export PATH=~/bin:$PATH
 export PATH=./bin:$PATH
