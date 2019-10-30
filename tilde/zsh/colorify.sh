74da394546dadaf4b84935984c614d00c6bc7ce4
Bodo Tasche <bodo@wannawork.de>
2019-10-30 21:18:28 +0100

    added java home path and fixed autocompletion of colourify

--- tilde/zsh/colorify.sh
+++ tilde/zsh/colorify.sh
@@ -10,16 +10,43 @@ if [ -x "$(command -v grc)" ]; then
   # GRC add color the output (thanks to @klaustopher)
   export GRC=`which grc`
 
-  alias colourify="$GRC -es --colour=auto"
-  alias configure='colourify ./configure'
-  alias diff='colourify diff'
-  alias make='colourify make'
-  alias gcc='colourify gcc'
-  alias g++='colourify g++'
-  alias as='colourify as'
-  alias gas='colourify gas'
-  alias ld='colourify ld'
-  alias netstat='colourify netstat'
-  alias ping='colourify ping'
-  alias traceroute='colourify /usr/sbin/traceroute'
+  colourify() {
+    $GRC -es --colour=auto "$@"
+  }
+
+  configure() {
+    colourify ./configure "$@"
+  }
+  
+  diff() {
+    colourify diff "$@"
+  }
+
+  make () {
+    colourify make "$@"
+  }
+  gcc() {
+    colourify gcc "$@"
+  }
+  g++() {
+    colourify g++ "$@"
+  }
+  as() {
+    colourify as "$@"
+  }
+  gas() {
+    colourify gas "$@"
+  }
+  ld() {
+    colourify ld "$@"
+  }
+  netstat() {
+    colourify netstat "$@"
+  }
+  ping() {
+    colourify ping "$@"
+  }
+  traceroute() {
+    colourify /usr/sbin/traceroute "$@"
+  }
 fi
