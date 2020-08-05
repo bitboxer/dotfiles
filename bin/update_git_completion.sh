eba4be2fdf27de562fef5ae6f7fedbda28d2c1c9
Bodo Tasche <bodo@wannawork.de>
2020-08-05 12:18:52 +0200

    using the official git completions

---
+++ bin/update_git_completion.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh > ~/code/dotfiles/tilde/zsh/completions/git-completion.zsh
--- bin/update_mac.sh
+++ bin/update_mac.sh
@@ -4,4 +4,6 @@ brew update
 brew upgrade
 brew cleanup
 
+./update_git_completions.sh
+
 nvim +PlugUpdate +UpdateRemotePlugins +qa
