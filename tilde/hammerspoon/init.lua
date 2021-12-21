f8381beaa78b8d7511421e688c269a250913a2e4
Bodo Tasche <bodo@wannawork.de>
2021-12-21 20:50:14 +0100

    Improved keyboard by detecting on wakeup configuration
    thanks to @fhemberger

--- tilde/hammerspoon/init.lua
+++ tilde/hammerspoon/init.lua
@@ -1,15 +1,41 @@
-function configureKeyboard(data)
-    local isKeyboardAffected = data.vendorID == 12951 and data.productID == 6505
-    if isKeyboardAffected and data.eventType == "added" then
-        hs.keycodes.setLayout("U.S.")
+local usbLogger = hs.logger.new('usb', 'debug')
+
+function isExternalKeyboard(usbDevice)
+  -- usbLogger.df("pname %s, vname %s, vId %s, pId %s", usbDevice.productName, usbDevice.vendorName, usbDevice.vendorID, usbDevice.productID)
+  return usbDevice.vendorID == 12951 and usbDevice.productID == 6505
+end
+
+function configureKeyboard(event)
+  if isExternalKeyboard(event) and event.eventType == "added" then
+    hs.keycodes.setLayout("U.S.")
+  end
+  if isExternalKeyboard(event) and event.eventType == "removed" then
+    hs.keycodes.setLayout("Deutsch (Programmieren)")
+  end
+end
+
+function checkKeyboardOnWakeup(event)
+  if (event == hs.caffeinate.watcher.screensDidWake) then
+    local usbDevices = hs.usb.attachedDevices()
+    if usbDevices == nil then
+      return
     end
-    if isKeyboardAffected and data.eventType == "removed" then
-        hs.keycodes.setLayout("Deutsch (Programmieren)")
+
+    local keyboardLayout = "Deutsch (Programmieren)"
+    for index, usbDevice in pairs(usbDevices) do
+      if isExternalKeyboard(usbDevice) then
+        keyboardLayout = "U.S."
+      end
     end
+    hs.keycodes.setLayout(keyboardLayout)
+  end
 end
 
 local keyboardWatcher = hs.usb.watcher.new(configureKeyboard)
 keyboardWatcher:start()
+
+local wakeupWatcher = hs.caffeinate.watcher.new(checkKeyboardOnWakeup)
+wakeupWatcher:start()
 
 wifiWatcher = nil
 homeSSID = "bighappymushylump"
