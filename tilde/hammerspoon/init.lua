89f93e2bfe8e4b1c878f0f24c5b9a2a10356818b
Bodo Tasche <bodo@wannawork.de>
2023-03-06 22:51:34 +0100

    wlan should start with the name, no exact match needed

--- tilde/hammerspoon/init.lua
+++ tilde/hammerspoon/init.lua
@@ -1,34 +1,38 @@
 local usbLogger = hs.logger.new('usb', 'debug')
 
 function isExternalKeyboard(usbDevice)
-  -- usbLogger.df("pname %s, vname %s, vId %s, pId %s", usbDevice.productName, usbDevice.vendorName, usbDevice.vendorID, usbDevice.productID)
-  return usbDevice.vendorID == 12951 and usbDevice.productID == 6505
+    -- usbLogger.df("pname %s, vname %s, vId %s, pId %s", usbDevice.productName, usbDevice.vendorName, usbDevice.vendorID, usbDevice.productID)
+    return usbDevice.vendorID == 12951 and usbDevice.productID == 6505
 end
 
 function configureKeyboard(event)
-  if isExternalKeyboard(event) and event.eventType == "added" then
-    hs.keycodes.setLayout("U.S.")
-  end
-  if isExternalKeyboard(event) and event.eventType == "removed" then
-    hs.keycodes.setLayout("Deutsch (Programmieren)")
-  end
+    if isExternalKeyboard(event) and event.eventType == "added" then
+        hs.keycodes.setLayout("U.S.")
+    end
+    if isExternalKeyboard(event) and event.eventType == "removed" then
+        hs.keycodes.setLayout("Deutsch (Programmieren)")
+    end
 end
 
 function checkKeyboardOnWakeup(event)
-  if (event == hs.caffeinate.watcher.screensDidWake) then
-    local usbDevices = hs.usb.attachedDevices()
-    if usbDevices == nil then
-      return
-    end
+    if (event == hs.caffeinate.watcher.screensDidWake) then
+        local usbDevices = hs.usb.attachedDevices()
+        if usbDevices == nil then
+            return
+        end
 
-    local keyboardLayout = "Deutsch (Programmieren)"
-    for index, usbDevice in pairs(usbDevices) do
-      if isExternalKeyboard(usbDevice) then
-        keyboardLayout = "U.S."
-      end
+        local keyboardLayout = "Deutsch (Programmieren)"
+        for index, usbDevice in pairs(usbDevices) do
+            if isExternalKeyboard(usbDevice) then
+                keyboardLayout = "U.S."
+            end
+        end
+        hs.keycodes.setLayout(keyboardLayout)
     end
-    hs.keycodes.setLayout(keyboardLayout)
-  end
+end
+
+function string.starts(String, Start)
+    return string.sub(String, 1, string.len(Start)) == Start
 end
 
 local keyboardWatcher = hs.usb.watcher.new(configureKeyboard)
@@ -44,7 +48,7 @@ lastSSID = hs.wifi.currentNetwork()
 function ssidChangedCallback()
     newSSID = hs.wifi.currentNetwork()
 
-    if newSSID == homeSSID and lastSSID ~= homeSSID then
+    if string.starts(newSSID, homeSSID) and string.starts(lastSSID, homeSSID) then
         -- We just joined our home WiFi network
         hs.network.configuration.open():setLocation("Home")
     elseif newSSID ~= homeSSID and lastSSID == homeSSID then
