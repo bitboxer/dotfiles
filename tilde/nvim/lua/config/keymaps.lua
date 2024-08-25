ffdcd7d0873a5e3ee2b19925284678ef0732ef4f
Bodo Tasche <bodo@wannawork.de>
2024-08-25 20:32:55 +0200

    Trying out lazyvim
    I was bored with my neovim setup, and I thought maybe
    I should stop fiddeling around with a half broken thing
    and go to the thing that cool kids use. a neovim distribution.
    
    After checking out several, I landed on lazyvim. Let's see
    if I continue to like it.

---
+++ tilde/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+vim.keymap.set("n", "<C-p>", function()
+  require("telescope.builtin").find_files()
+end, { desc = "Find files" })
+
+vim.keymap.set("n", "<leader>ghm", function()
+  local gs = package.loaded.gitsigns
+  gs.diffthis("main")
+end, { desc = "Diff this against main" })
