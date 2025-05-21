6cfc8559da439094c5ce0c58d16849c64f8908bb
Bodo Tasche <bodo@wannawork.de>
2025-05-21 21:51:58 +0200

    Moving to codeberg

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# This script is used by gitpod to setup the environment
-./bin/setup_linux_min.sh
--- tilde/nvim/lua/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  enabled = true,
-  opts = function(_, opts)
-    opts.sections.lualine_c[4] = { "filename", path = 1 }
-  end,
-}
